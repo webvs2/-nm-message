54ca37188f5ed5346e822477eb8d5172f9f6ce4e
yangpengfei1 <2636098325@qq.com>
2024-07-25 10:10:41 +0800

    Workflow preparation

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
    mode:"production",
+   base:'/-nm-message/',
    build:{
     target:"modules",
     outDir:'docs',
-    
+
    }
   })
